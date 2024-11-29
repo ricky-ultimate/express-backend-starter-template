8d92e920bcf8fd8af2d88d794738ed3d2e9f0c83
justcovenant <justcovenant@outlook.com>
2024-11-29 22:42:18 +0100

    feat: created starter template base

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import morgan from "morgan"
+import helmet from "helmet"
+import { ENV } from "./constants/env"
+import logger from "./utils/logger.utils"
+
+dotenv.config()
+
+const app = express();
+
+app.use(cors())
+app.use(helmet())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(morgan("dev"))
+
+
+app.listen(ENV.PORT, () => {
+    logger("Welcome to AnimeNexa 🚀🚀🚀")
+} )
